f68d3bab4d0a3cdec8a855e956796631860ad6a4
Kat <katarzynaa89@gmail.com>
2024-01-26 12:00:58 +0000

    Fixed a bug, gigs showing in favorited and in all

--- src/components/gigContainer.jsx
+++ src/components/gigContainer.jsx
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Gig from './Gig';
-// import './FavouriteGigContainer.css'; // Create a CSS file for styling if needed
 import Carbons from '../assets/Carbons.png'
 import Undercover from '../assets/Undercover-hippie.avif'
 import Shpongle from '../assets/shpongle.jpeg'
 
 const GigList = () => {
     const [gigs, setGigs] = useState([]);
-    // This state variable favoritedGigs is an array that holds the gigs marked as favorites. The isFavorited property is used to determine whether a specific gig should be included in this array.    
     const [favoritedGigs, setFavoritedGigs] = useState([]);
-    const [loading, setLoading] = useState(true); // Add loading state
+    const [loading, setLoading] = useState(true); // Loading state - when waiting for the fetch response
 
     useEffect(() => {
         const URL = `https://makers-gig-backend.onrender.com/events`;
@@ -26,16 +24,17 @@ const GigList = () => {
             });
     }, []);
 
-    // Create a list with all gigs and their states, if a user click like, this function will be triggered and will change the state of that gig inside of the list
+    // toggleFavourite will essentially create a new list from Setgigs(gigs) with one gig (gigId) toggled
     const toggleFavorite = (gigId) => {
-        setGigs((prevGigs) =>
-            prevGigs.map((gig) =>
+        setGigs((gigs) => // state value of gigs and returns a new state value based on the provided logic.
+            gigs.map((gig) =>
                 gig.event_id === gigId ? { ...gig, isFavorited: !gig.isFavorited } : gig
             )
         );
     };
 
-    // filter updatedGigs to create a new array with only favourited gigs
+    // Filter gigs array to create a new updatedFavouritedGigs array containing only favorited gigs
+    // useEffect used to control re-renders for only when the state is changed, otherwise create an infinite re-render loop
     useEffect(() => {
         const updatedFavoritedGigs = gigs.filter((gig) => gig.isFavorited);
         setFavoritedGigs(updatedFavoritedGigs);
@@ -49,7 +48,7 @@ const GigList = () => {
             ))}
 
             <h2>All Gigs</h2>
-            {gigs.map((gig) => !gig.isFavorited && <Gig key={gig.event_id} {...gig} onToggleFavorite={() => toggleFavorite(gig.event_id)} />)}
+            {gigs.map((gig) => <Gig key={gig.event_id} {...gig} onToggleFavorite={() => toggleFavorite(gig.event_id)} />)}
         </div>
     );
 };
